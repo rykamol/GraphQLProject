a08f42b52d6d07eec8dc5478e6b5a27e3437f70b
rykamol <kamolroy.din@gmail.com>
2025-06-23 23:28:19 +0600

    Added menu class and interface.

---
+++ GraphQLProject/Interfaces/IMenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using GraphQLProject.Models;
+
+namespace GraphQLProject.Interfaces
+{
+    public interface IMenuRepository
+    {
+        List<Menu> GetAllMenus();
+        Menu GetMenuById(int id);
+        Menu AddMenu(Menu name);
+        Menu UpdateMenu(Menu menu);
+        void DeleteMenu(int id);
+    }
+}
