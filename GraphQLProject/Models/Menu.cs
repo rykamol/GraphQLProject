637a5a88bf7d0e1a6baa15245e69b7e4e89f5c2b
rykamol <kamolroy.din@gmail.com>
2025-07-13 00:06:24 +0600

    Adding restaurant models and modify menu class

--- GraphQLProject/Models/Menu.cs
+++ GraphQLProject/Models/Menu.cs
@@ -6,5 +6,8 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public double Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
