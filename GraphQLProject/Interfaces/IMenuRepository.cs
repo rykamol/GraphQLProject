fde79cf0d62e8332cb6e4ec78cd4b331a0f65b73
rykamol <kamolroy.din@gmail.com>
2025-07-12 16:31:30 +0600

    Added Menu Repository

--- GraphQLProject/Interfaces/IMenuRepository.cs
+++ GraphQLProject/Interfaces/IMenuRepository.cs
@@ -7,7 +7,7 @@ namespace GraphQLProject.Interfaces
         List<Menu> GetAllMenus();
         Menu GetMenuById(int id);
         Menu AddMenu(Menu name);
-        Menu UpdateMenu(Menu menu);
+        Menu UpdateMenu(int id,Menu menu);
         void DeleteMenu(int id);
     }
 }
