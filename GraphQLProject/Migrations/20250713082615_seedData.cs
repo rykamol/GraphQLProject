b90a6ad08132c84ad66a375af1ad41f63dd61e63
rykamol <kamolroy.din@gmail.com>
2025-07-13 14:30:52 +0600

    Seed data

---
+++ GraphQLProject/Migrations/20250713082615_seedData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GraphQLProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "ImageUrl", "Name" },
+                values: new object[,]
+                {
+                    { 1, "https://example.com/categories/appetizers.jpg", "Appetizers" },
+                    { 2, "https://example.com/categories/main-course.jpg", "Main Course" },
+                    { 3, "https://example.com/categories/desserts.jpg", "Desserts" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reservations",
+                columns: new[] { "Id", "CustomerName", "Email", "PartySize", "PhoneNumber", "ReservationDate", "SpecialRequest" },
+                values: new object[,]
+                {
+                    { 1, "John Doe", "johndoe@example.com", 2, "555-123-4567", new DateTime(2025, 7, 20, 14, 26, 14, 978, DateTimeKind.Local).AddTicks(4259), "No nuts in the dishes, please." },
+                    { 2, "Jane Smith", "janesmith@example.com", 4, "555-987-6543", new DateTime(2025, 7, 23, 14, 26, 14, 978, DateTimeKind.Local).AddTicks(4274), "Gluten-free options required." },
+                    { 3, "Michael Johnson", "michaeljohnson@example.com", 6, "555-789-0123", new DateTime(2025, 7, 27, 14, 26, 14, 978, DateTimeKind.Local).AddTicks(4276), "Celebrating a birthday." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Menus",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Spicy chicken wings served with blue cheese dip.", "https://example.com/menus/chicken-wings.jpg", "Chicken Wings", 9.9900000000000002 },
+                    { 2, 2, "Grilled steak with mashed potatoes and vegetables.", "https://example.com/menus/steak.jpg", "Steak", 24.5 },
+                    { 3, 3, "Decadent chocolate cake with a scoop of vanilla ice cream.", "https://example.com/menus/chocolate-cake.jpg", "Chocolate Cake", 6.9500000000000002 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Menus",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Menus",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Menus",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
