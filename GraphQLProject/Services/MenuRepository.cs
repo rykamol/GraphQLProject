2788fa27c170330c11f4927069fd7df00922ff27
rykamol <kamolroy.din@gmail.com>
2025-07-13 15:45:22 +0600

    use dbcontext for menu service

--- GraphQLProject/Services/MenuRepository.cs
+++ GraphQLProject/Services/MenuRepository.cs
@@ -1,4 +1,5 @@
-﻿using GraphQLProject.Interfaces;
+﻿using GraphQLProject.Data;
+using GraphQLProject.Interfaces;
 using GraphQLProject.Models;
 
 namespace GraphQLProject.Services
@@ -8,40 +9,45 @@ namespace GraphQLProject.Services
     //interfaces which grately enhance the maintability of the code.
     public class MenuRepository : IMenuRepository
     {
-        private static List<Menu> MenuList = new List<Menu>()
-        {
-            new Menu() { Id = 1, Name = "Classic Burger", Description="A juicy chicken burger with lettuce and cheese" , Price = 8.99},
-            new Menu() { Id = 2, Name = "Margherita Pizza", Description = "Tomato, mozzarella, and basil pizza", Price = 10.50 },
-            new Menu() { Id = 3, Name = "Grilled Chicken Salad", Description = "Fresh garden salad with grilled chicken", Price = 7.95 },
-            new Menu() { Id = 4, Name = "Pasta Alfredo", Description = "Creamy Alfredo sauce with fettuccine pasta", Price = 12.75 },
-            new Menu() { Id = 5, Name = "Chocolate Brownie Sundae", Description = "Warm chocolate brownie with ice cream and fudge", Price = 6.99 },
+        private GraphQLDbContext _dbContext;
 
-        };
+        public MenuRepository(GraphQLDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         public Menu AddMenu(Menu menu)
         {
-            MenuList.Add(menu);
+            _dbContext.Menus.Add(menu);
+            _dbContext.SaveChanges();
             return menu;
         }
 
         public void DeleteMenu(int id)
         {
-            MenuList.RemoveAt(id);
+            var deleteMenu = _dbContext.Menus.FirstOrDefault(x => x.Id == id);
+            _dbContext.Menus.Remove(deleteMenu);
         }
 
         public List<Menu> GetAllMenus()
         {
-            return MenuList;
+            return _dbContext.Menus.ToList();
         }
 
         public Menu GetMenuById(int id)
         {
-            return MenuList.Find(x => x.Id == id);
+            return _dbContext.Menus.FirstOrDefault(x => x.Id == id);
         }
 
         public Menu UpdateMenu(int id, Menu menu)
         {
-            MenuList[id] = menu;
+            var updateMenu = _dbContext.Menus.FirstOrDefault(x => x.Id == id);
+            updateMenu.Name = menu.Name;
+            updateMenu.Description = menu.Description;
+            updateMenu.Price = menu.Price;
+            updateMenu.ImageUrl = menu.ImageUrl;
+            updateMenu.CategoryId = menu.CategoryId;
+
             return menu;
         }
     }
