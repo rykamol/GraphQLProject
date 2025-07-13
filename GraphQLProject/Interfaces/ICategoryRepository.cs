53ae4f8f78cb93c23bb3c62835676cf7a70b26cc
rykamol <kamolroy.din@gmail.com>
2025-07-13 13:00:02 +0600

    Added Reservation interfaces.

---
+++ GraphQLProject/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using GraphQLProject.Models;
+
+namespace GraphQLProject.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAllCategories();
+        Category AddCategory(Category category);
+        Category UpdateCategory(int menuId, Category category);
+        void DeleteCategory(int id);
+    }
+}
