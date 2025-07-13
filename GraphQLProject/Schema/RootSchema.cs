24b6ffad30a2a420cd86c8bac65bb127781d1471
rykamol <kamolroy.din@gmail.com>
2025-07-13 14:17:45 +0600

    Dealing with the root query

---
+++ GraphQLProject/Schema/RootSchema.cs
@@ -0,0 +1,14 @@
+﻿using GraphQLProject.Query;
+
+namespace GraphQLProject.Schema
+{
+    public class RootSchema: GraphQL.Types.Schema
+    {
+        //IServiceProvider is built in .net core that provides access
+        //to all the registered services in the app dependency inj container.
+        public RootSchema(IServiceProvider serviceProvider):base(serviceProvider)
+        {
+            Query = serviceProvider.GetRequiredService<RootQuery>();
+        }
+    }
+}
